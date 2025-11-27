72cd87b6692a8377a6c955f556f481fa910aeeb7
Rudr7_7 <ankitgautam50366@gmail.com>
2025-11-27 23:59:01 +0530

    setting up basic express server

--- 03_Express.js_basics/script.js
+++ 03_Express.js_basics/script.js
@@ -14,3 +14,25 @@ It helps you build APIs and web applications easily.
 • Handles HTTP requests & responses
 • Great for REST APIs
 */
+
+/*
+2. SETTING UP A BASIC EXPRESS APP
+---------------------------------------------------
+Steps:
+1. Install Express →  npm install express
+2. Create app.js
+3. Setup server
+*/
+
+const express = require('express');   // Import express
+const app = express();                // Create app instance
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Nonchalant Sigma  boi !!!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

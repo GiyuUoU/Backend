3f433d57306ea61b100a1f2045aa49ae4b9f6535
Rudr7_7 <ankitgautam50366@gmail.com>
2025-11-27 23:52:23 +0530

    intro to Express.js

---
+++ 03_Express.js_basics/script.js
@@ -0,0 +1,16 @@
+/*
+---------------------------------------------------
+EXPRESS.JS NOTES
+---------------------------------------------------
+*/
+
+/*
+1. INTRODUCTION TO EXPRESS.JS
+---------------------------------------------------
+Express.js is a lightweight, fast, and flexible backend framework for Node.js.
+It helps you build APIs and web applications easily.
+• Simplifies routing
+• Supports middleware
+• Handles HTTP requests & responses
+• Great for REST APIs
+*/
